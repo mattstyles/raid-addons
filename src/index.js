d4ddba3daabfaa705611910b909eef8becd58c38
mattstyles <matt@veryfizzyjelly.com>
2016-04-15 21:33:13 +0100

    #add click signal

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import curry from './curry'
 import compose from './compose'
 import TickSignal from './tick'
 import KeySignal from './keys'
+import ClickSignal from './click'
 
 var addons = {
   curry,
   compose,
   TickSignal,
-  KeySignal
+  KeySignal,
+  ClickSignal
 }
 
 export default addons
