f89f6a83cf91469fe61150c61fa632cc7231dcaf
mattstyles <matt@veryfizzyjelly.com>
2016-04-15 20:59:24 +0100

    #update keys example

--- src/keys.js
+++ src/keys.js
@@ -3,9 +3,20 @@ import Quay from 'quay'
 import { Signal } from 'raid'
 
 /**
- * Key emitter is a global keypress handler
+ * Key emitter is a global keypress handler.
  * Emits regular events from keypresses passing back a map
- * of the currently pressed keys
+ * of the currently pressed keys.
+ *
+ * @example
+ * const keys = TickSignal({
+ *   model: app
+ * })
+ * keys.register( src => {
+ *   src.subscribe( event => {
+ *     console.log( event.keys.has( '<up>' ) )
+ *   })
+ * })
+ *
  */
 export default function KeySignal( opts ) {
   const quay = new Quay()
