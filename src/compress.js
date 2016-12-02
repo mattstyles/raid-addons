27d21c05eaf97eb3b1dff5f0849d0a1225b47b9b
Matt Styles <matt@personalurban.com>
2016-12-02 06:37:35 +0000

    add flow

--- src/compress.js
+++ src/compress.js
@@ -1,8 +1,9 @@
 
 const compress = updates => {
   return (state, event) => {
-    if (updates[event.type]) {
-      return updates[event.type](state)
+    let update = updates[event.type]
+    if (update) {
+      return update(state, event.payload)
     }
     return state
   }
